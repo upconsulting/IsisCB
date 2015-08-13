6b67b05e2a543f0f8684ba8ec8bfd3d893bbc470
Erick Peirson <erick.peirson@asu.edu>
2015-08-12 20:52:57 -0700

    [#101086536] working on AWS deployment

--- awsdeploy/ApplicationStart.sh
+++ awsdeploy/ApplicationStart.sh
@@ -1,3 +1,8 @@
+cd /home/ec2-user/isiscb
+
+export WORKON_HOME=$HOME/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+
 workon isiscb
 
 supervisorctl -c /etc/supervisor/conf.d/supervisor.conf start isiscb
