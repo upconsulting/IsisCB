90e592fb9cf2e8854643b8dfd5a232d644a46e2f
Erick Peirson <erick.peirson@asu.edu>
2015-09-07 06:52:09 -0700

    added HAYSTACK_CONNECTIONS to local_settings

--- isiscb/isiscb/local_settings.py
+++ isiscb/isiscb/local_settings.py
@@ -77,6 +77,14 @@ DATABASES = {
     }
 }
 
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'haystack',
+    },
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
