0effc64a3db9a9bd193e912995f85af34d3e1813
Erick Peirson <erick.peirson@asu.edu>
2015-09-07 19:32:37 -0700

    updated settings

--- isiscb/isiscb/development_settings.py
+++ isiscb/isiscb/development_settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'isiscb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- isiscb/isiscb/local_postgresql_settings.py
+++ isiscb/isiscb/local_postgresql_settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'isiscb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
