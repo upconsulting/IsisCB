a5544cfe393294f8dada84c8ddd93bcc51cf77af
Julia Damerow <julia.damerow@googlemail.com>
2015-09-18 07:19:30 -0700

    [#103594060] 'home' shows search page instead of index page

--- isiscb/isisdata/urls.py
+++ isiscb/isisdata/urls.py
@@ -11,7 +11,8 @@ from . import views
 sqs = SearchQuerySet().facet('authors').facet('subjects')
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    #url(r'^$', views.index, name='index'),
+    url(r'^$', IsisSearchView(form_class=MyFacetedSearchForm, searchqueryset=sqs), name='index'),
     url(r'^(?P<obj_id>[A-Z]+[0-9]+)/$', views.index, name='index'),
     url(r'^authority/(?P<authority_id>[A-Z]+[0-9]+)/$', views.authority, name='authority'),
     url(r'^citation/(?P<citation_id>[A-Z]+[0-9]+)/$', views.citation, name='citation'),
