b386930660d22a4a294b3ea52de8cb576dd28088
Erick Peirson <erick.peirson@asu.edu>
2015-10-09 08:47:39 -0700

    elasticbeanstalk configuration

---
+++ isiscb/isisdata/management/commands/createsu.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username="admin").exists():
+            password = os.environ.get('DJANGO_ADMIN_PASSWORD')
+            User.objects.create_superuser("admin", "admin@admin.com", password)
