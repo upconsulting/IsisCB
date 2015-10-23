1d04c91185074f2325c4622d8f102cb84a23c770
Erick Peirson <erick.peirson@asu.edu>
2015-10-23 15:58:25 -0700

    codedeploy config

--- awsdeploy/ApplicationStart.sh
+++ awsdeploy/ApplicationStart.sh
@@ -8,7 +8,16 @@ workon isiscb
 touch /home/ec2-user/isiscb/isiscb/server_start
 chmod 666 /home/ec2-user/isiscb/isiscb/server_start
 
-awsdeploy/bin/set_environ.sh
+INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id);
+aws ec2 describe-tags --filters "Name=resource-id,Values=i-a5accd63" "Name=key,Values=RDS_USER" > rds_user.json
+aws ec2 describe-tags --filters "Name=resource-id,Values=i-a5accd63" "Name=key,Values=RDS_PASSWORD" > rds_password.json
+aws ec2 describe-tags --filters "Name=resource-id,Values=i-a5accd63" "Name=key,Values=AWS_ACCESS_KEY" > rds_access.json
+aws ec2 describe-tags --filters "Name=resource-id,Values=i-a5accd63" "Name=key,Values=AWS_SECRET_ACCESS_KEY" > rds_secret.json
+export RDS_USER=$(python awsdeploy/bin/get_environ.py rds_user.json);
+export RDS_PASSWORD=$(python awsdeploy/bin/get_environ.py rds_password.json);
+export AWS_ACCESS_KEY=$(python awsdeploy/bin/get_environ.py rds_access.json);
+export AWS_SECRET_ACCESS_KEY=$(python awsdeploy/bin/get_environ.py rds_secret.json);
+
 
 # Supervisor manages gunicorn. See awsdeploy/supervisor.conf.
 supervisorctl -c /etc/supervisor/conf.d/supervisor.conf start isiscb
