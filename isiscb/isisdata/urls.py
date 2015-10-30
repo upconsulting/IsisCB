876590b596874481bc69ac69f26f0b0ff5413d07
Erick Peirson <erick.peirson@asu.edu>
2015-10-30 13:51:10 -0700

    ISISCB-153 changed facet limit to 300 for all facets

--- isiscb/isisdata/urls.py
+++ isiscb/isisdata/urls.py
@@ -8,31 +8,31 @@ from isisdata.views import IsisSearchView
 
 from . import views
 
-sqs = SearchQuerySet().facet('authors'). \
-        facet('type'). \
-        facet('publication_date'). \
-        facet('persons'). \
-        facet('categories'). \
-        facet('subjects'). \
-        facet('editors'). \
-        facet('advisors'). \
-        facet('translators'). \
-        facet('publishers'). \
-        facet('schools'). \
-        facet('institutions'). \
-        facet('meetings'). \
-        facet('periodicals'). \
-        facet('book_series'). \
-        facet('time_periods'). \
-        facet('geographics'). \
-        facet('people'). \
-        facet('subject_institutions'). \
-        facet('serial_publiations'). \
-        facet('classification_terms'). \
-        facet('concepts'). \
-        facet('creative_works'). \
-        facet('events'). \
-        facet('authority_type')
+sqs = SearchQuerySet().facet('authors', size=300). \
+        facet('type', size=300). \
+        facet('publication_date', size=300). \
+        facet('persons', size=300). \
+        facet('categories', size=300). \
+        facet('subjects', size=300). \
+        facet('editors', size=300). \
+        facet('advisors', size=300). \
+        facet('translators', size=300). \
+        facet('publishers', size=300). \
+        facet('schools', size=300). \
+        facet('institutions', size=300). \
+        facet('meetings', size=300). \
+        facet('periodicals', size=300). \
+        facet('book_series', size=300). \
+        facet('time_periods', size=300). \
+        facet('geographics', size=300). \
+        facet('people', size=300). \
+        facet('subject_institutions', size=300). \
+        facet('serial_publiations', size=300). \
+        facet('classification_terms', size=300). \
+        facet('concepts', size=300). \
+        facet('creative_works', size=300). \
+        facet('events', size=300). \
+        facet('authority_type', size=300)
 
 urlpatterns = [
     #url(r'^$', views.index, name='index'),
