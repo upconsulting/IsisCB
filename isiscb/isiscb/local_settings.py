b34a5f407fd41efa9d99c8ce34c9f58b9b2f8697
Erick Peirson <erick.peirson@asu.edu>
2016-01-26 21:59:17 -0700

    local settings

--- isiscb/isiscb/local_settings.py
+++ isiscb/isiscb/local_settings.py
@@ -147,6 +147,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
