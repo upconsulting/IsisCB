9b59362f8e2b0c00c6f827c3acb69c368b044c47
Julia Damerow <julia.damerow@googlemail.com>
2016-03-07 09:27:26 -0700

    [ISISCB-344] Added more types.

--- isiscb/isisdata/helpers/linked_data.py
+++ isiscb/isisdata/helpers/linked_data.py
@@ -1,11 +1,11 @@
 from rdflib import Graph, URIRef, BNode, Literal
-from rdflib.namespace import RDF, FOAF, DC
+from rdflib.namespace import RDF, FOAF, DC, Namespace
 import urllib
 
 def generate_rdf(authority):
     g = Graph()
 
-    auth = URIRef("http://data.isiscb.org/isis/" + authority.id) #urllib.quote(authority.name.replace(" ", "_")))
+    auth = URIRef("http://data.isiscb.org/authority/" + authority.id) #urllib.quote(authority.name.replace(" ", "_")))
 
     type = get_type(authority.type_controlled)
     if not type:
@@ -14,14 +14,17 @@ def generate_rdf(authority):
     return g.serialize(format='application/rdf+xml')
 
 def get_type(authority_type):
+    mesh = Namespace('http://id.nlm.nih.gov/mesh/vocab#')
+    isisns = Namespace('http://data.isiscb.org/vocab#')
+
     type_dict = {}
     type_dict['PE'] = FOAF.Person
     type_dict['IN'] = FOAF.Institution
     type_dict['TI'] = DC.PeriodOfTime
     type_dict['GE'] = DC.Coverage
-    type_dict['SE'] = None
-    type_dict['CT'] = None
-    type_dict['CO'] = None
+    type_dict['SE'] = mesh.PublicationType
+    type_dict['CT'] = isisns.ClassificationTerm
+    type_dict['CO'] = isisns.Concept
     type_dict['CW'] = None
     type_dict['EV'] = None
     type_dict['CR'] = None
