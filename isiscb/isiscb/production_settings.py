323b5e98009edb989875bd3bde17ae57e5c81bfe
Erick Peirson <erick.peirson@asu.edu>
2016-03-24 11:59:17 -0400

    production settings

--- isiscb/isiscb/production_settings.py
+++ isiscb/isiscb/production_settings.py
@@ -22,8 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DEBUG', False))
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'isiscb-staging.elasticbeanstalk.com',
