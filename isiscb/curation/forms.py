ecac1676b8c3b7e1b90c96ac27c9b38042da7837
Erick Peirson <erick.peirson@asu.edu>
2016-06-27 16:18:55 -0400

    curator can edit citation details ISISCB-463

---
+++ isiscb/curation/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+
+from isisdata.models import *
+
+
+class PartDetailsForm(forms.ModelForm):
+    extent_note = forms.CharField(widget=forms.widgets.Textarea({'rows': '1'}))
+
+    class Meta:
+        model = PartDetails
+        fields = '__all__'
+
+
+class CitationForm(forms.ModelForm):
+    abstract = forms.CharField(widget=forms.widgets.Textarea({'rows': '3'}), required=False)
+    description = forms.CharField(widget=forms.widgets.Textarea({'rows': '3'}), required=False)
+
+    additional_titles = forms.CharField(widget=forms.widgets.Textarea({'rows': '2'}), required=False)
+    edition_details = forms.CharField(widget=forms.widgets.Textarea({'rows': '2'}), required=False)
+    physical_details = forms.CharField(widget=forms.widgets.Textarea({'rows': '2'}), required=False)
+
+    class Meta:
+        model = Citation
+        fields = ['type_controlled', 'title', 'description', 'edition_details',
+                  'physical_details', 'language', 'abstract',
+                  'additional_titles', 'book_series']
