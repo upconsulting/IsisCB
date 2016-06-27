e21e997c66f1cdf1770f227141a77f3f81bd9b81
Erick Peirson <erick.peirson@asu.edu>
2016-06-27 11:35:17 -0400

    ISISCB-454 ICICB-455 staff users can see lists of authority and citation records, and filter htem

--- isiscb/curation/filters.py
+++ isiscb/curation/filters.py
@@ -1,11 +1,47 @@
 import django_filters
+from django_filters.fields import Lookup
 
-from isisdata.models import Citation
+
+from isisdata.models import *
+import six
+import iso8601
 
 
-class Citation(filters.FilterSet):
-    name = django_filters.CharFilter(name='name', lookup_type='icontains')
+class CitationFilter(django_filters.FilterSet):
+    id = django_filters.CharFilter(name='id', lookup_type='exact')
+    title = django_filters.CharFilter(name='title', lookup_type='icontains')
+    type_controlled = django_filters.ChoiceFilter(choices=[('', 'All')] + list(Citation.TYPE_CHOICES))
+    publication_date_from = django_filters.MethodFilter()
+    publication_date_to = django_filters.MethodFilter()
+    abstract = django_filters.CharFilter(name='abstract', lookup_type='icontains')
+    description = django_filters.CharFilter(name='description', lookup_type='icontains')
 
     class Meta:
         model = Citation
-        fields = ['name',]
+        fields = ['id', 'title', 'publication_date_from', 'publication_date_to',
+                  'abstract', 'description', ]
+
+    def filter_publication_date_from(self, queryset, value):
+        try:
+            date = iso8601.parse_date(value)
+        except:
+            return queryset
+        return queryset.filter(publication_date__gte=date)
+
+    def filter_publication_date_to(self, queryset, value):
+        try:
+            date = iso8601.parse_date(value)
+        except:
+            return queryset
+        return queryset.filter(publication_date__lte=date)
+
+
+class AuthorityFilter(django_filters.FilterSet):
+    id = django_filters.CharFilter(name='id', lookup_type='exact')
+    name = django_filters.CharFilter(name='name', lookup_type='icontains')
+    type_controlled = django_filters.ChoiceFilter(choices=[('', 'All')] + list(Authority.TYPE_CHOICES))
+    description = django_filters.CharFilter(name='description', lookup_type='icontains')
+
+    class Meta:
+        model = Authority
+        fields = ['name', 'type_controlled']
--- isiscb/curation/urls.py
+++ isiscb/curation/urls.py
@@ -5,4 +5,6 @@ from curation import views
 
 urlpatterns = [
     url(r'^(?i)dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^(?i)citation/$', views.citation, name='citation_list'),
+    url(r'^(?i)authority/$', views.authority, name='authority_list'),
 ]
