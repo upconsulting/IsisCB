efdc07777a618be91d088aa8eb5c600480e65215
Julia Damerow <julia.damerow@googlemail.com>
2016-06-30 16:39:43 -0700

    [ISISCB-537] show inactive records message in citation views and indicate inactive records in tables;

--- isiscb/curation/views.py
+++ isiscb/curation/views.py
@@ -676,6 +676,7 @@ def quick_and_dirty_authority_search(request):
         'description': obj.description,
         'datestring': _get_datestring_for_authority(obj),
         'url': reverse("curate_authority", args=(obj.id,)),
+        'public': obj.public,
     } for obj in queryset[:20]]
     return JsonResponse({'results': results})
 
