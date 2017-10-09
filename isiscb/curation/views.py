35e9bf2cf664a6f10823acb564212d71b2bf66e3
Julia Damerow <jdamerow@users.noreply.github.com>
2017-10-08 18:08:36 -0700

    [ISISCB-1038] added new page for deletion of duplicate linked data re… (#415)
    * [ISISCB-1038] added new page for deletion of duplicate linked data records
    
    * [ISISCB-1038] new page for deletion check

--- isiscb/curation/views.py
+++ isiscb/curation/views.py
@@ -1251,6 +1251,63 @@ def change_record_type(request, citation_id):
 
 @user_passes_test(lambda u: u.is_superuser or u.is_staff)
 @check_rules('can_access_view_edit', fn=objectgetter(Citation, 'citation_id'))
+def linkeddata_duplicates(request, citation_id):
+    citation = get_object_or_404(Citation, pk=citation_id)
+
+    context = {
+        'citation': citation,
+        'curation_section': 'datasets',
+        'curation_subsection': 'citations',
+        'search': request.GET.get('search', None),
+        'current': request.GET.get('current', None),
+    }
+
+    linkeddata_entries = citation.linkeddata_entries.all()
+    unique_entries = {}
+    duplicate_entries = []
+
+    for ld in linkeddata_entries:
+        key = str(ld.type_controlled) + "_" + str(ld.universal_resource_name)
+        if key not in unique_entries:
+            unique_entries[key] = ld
+        else:
+            duplicate_entries.append(ld)
+
+    context.update({
+        'unique_entries': unique_entries,
+        'duplicate_entries': duplicate_entries,
+    })
+
+    template = 'curation/citation_linkeddata_duplicates.html'
+
+    return render(request, template, context)
+
+
+@user_passes_test(lambda u: u.is_superuser or u.is_staff)
+@check_rules('can_access_view_edit', fn=objectgetter(Citation, 'citation_id'))
+def delete_linkeddata_duplicates(request, citation_id):
+    citation = get_object_or_404(Citation, pk=citation_id)
+
+    context = {
+        'citation': citation,
+        'curation_section': 'datasets',
+        'curation_subsection': 'citations',
+    }
+
+    target = reverse('curation:curate_citation', args=(citation_id,)) + '?tab=linkeddata'
+    if request.method == 'POST':
+        for ld_id in request.POST.get('delete_ids', '').split(','):
+            if ld_id.strip():
+                linkeddata = get_object_or_404(LinkedData, pk=ld_id)
+                linkeddata.delete()
+
+        if request.POST.get('search', None) and request.POST.get('current', None):
+            target += '&search=%s&current=%s' % (request.POST.get('search', None), request.POST.get('current', None))
+
+    return HttpResponseRedirect(target)
+
+@user_passes_test(lambda u: u.is_superuser or u.is_staff)
+@check_rules('can_access_view_edit', fn=objectgetter(Citation, 'citation_id'))
 def subjects_and_categories(request, citation_id):
     citation = get_object_or_404(Citation, pk=citation_id)
 
