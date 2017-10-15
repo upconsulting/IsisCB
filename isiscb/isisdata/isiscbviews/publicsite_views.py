305aa5b902de58d8ef27214c9f067887f3b3e247
Julia Damerow <julia.damerow@googlemail.com>
2017-10-15 15:35:31 -0700

    [ISISCB-1045] bugfix: don't stop when either authorities or citations are done

--- isiscb/isisdata/isiscbviews/publicsite_views.py
+++ isiscb/isisdata/isiscbviews/publicsite_views.py
@@ -25,13 +25,13 @@ def recent_records(request):
     start_index = 0
     end_index = nr_of_records
 
-    done = False
+    citations_done = False
+    authorities_done = False
     # unfortunately, citatations freshly created are public=False so we can't filter on that field when retrieving
     # creation events, we have to test that after we got the real object form the history object
-    while not done:
-        recent_citations = Citation.history.filter(history_type="+").order_by('-history_date')[start_index:end_index]
-        recent_authorities = Authority.history.filter(history_type="+").order_by('-history_date')[start_index:end_index]
-
+    recent_citations = Citation.history.filter(history_type="+").order_by('-history_date')[start_index:end_index] if not citations_done else []
+    recent_authorities = Authority.history.filter(history_type="+").order_by('-history_date')[start_index:end_index] if not authorities_done else []
+    while not ((citations_done or not recent_citations) and (authorities_done or recent_authorities)):
         lastMonth = timezone.now() - datetime.timedelta(days=days)
 
         for record in sorted(chain(recent_citations, recent_authorities), key=lambda rec: rec.history_date, reverse=True):
@@ -40,11 +40,15 @@ def recent_records(request):
                 if record.created_on > lastMonth:
                     recent_records.append(record)
                 else:
-                    done = True
-                    break
+                    if type(record) is HistoricalCitation:
+                        citations_done = True
+                    else:
+                        authorities_done = True
 
         start_index = end_index + 1
         end_index = end_index + 10
+        recent_citations = Citation.history.filter(history_type="+").order_by('-history_date')[start_index:end_index] if not citations_done else []
+        recent_authorities = Authority.history.filter(history_type="+").order_by('-history_date')[start_index:end_index] if not authorities_done else []
 
     context = {
         'active': 'home',
