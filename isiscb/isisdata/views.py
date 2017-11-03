b329fd78d5f81ee2d5f11576ac19710250be510f
Julia Damerow <julia.damerow@googlemail.com>
2017-11-02 21:19:16 -0700

    [ISISCB-1016] add diagram to authority page

--- isiscb/isisdata/views.py
+++ isiscb/isisdata/views.py
@@ -904,6 +904,13 @@ def authority(request, authority_id):
     }
     return render(request, 'isisdata/authority.html', context)
 
+def authority_author_timeline(request, authority_id):
+    data = {
+        'data1': [30, 200, 100, 400, 150, 250] ,
+        'data2': [50, 20, 10, 40, 15, 25]
+        }
+
+    return JsonResponse(data)
 
 def citation(request, citation_id):
     """
