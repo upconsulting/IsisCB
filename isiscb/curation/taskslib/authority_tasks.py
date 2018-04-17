7d35b9063740bbdd6d0998e83da46615e722d599
Julia Damerow <julia.damerow@googlemail.com>
2018-04-16 20:04:41 -0700

    [ISISCB-1021] catch unexpected exceptions and log them

--- isiscb/curation/taskslib/authority_tasks.py
+++ isiscb/curation/taskslib/authority_tasks.py
@@ -41,91 +41,96 @@ def add_attributes_to_authority(file_path, error_path, task_id):
         current_count = 0
         not_matching_subject_names = []
         errors = []
-        for row in csv.DictReader(f):
-            subject_id = row[COLUMN_NAME_ATTR_SUBJ_ID]
-            try:
-                authority = Authority.objects.get(pk=subject_id)
-            except Authority.DoesNotExist:
-                logger.error('Authority with id %s does not exist. Skipping attribute.' % (subject_id))
-                errors.append((subject_id, subject_id, 'Authority record does not exist.'))
-                current_count = update_count(current_count, task)
-                continue
-
-            related_name = row[COLUMN_NAME_ATTR_RELATED_NAME]
-            if authority.name != related_name:
-                not_matching_subject_names.append((subject_id, authority.name, related_name))
-
-            attribute_type = row[COLUMN_NAME_ATTR_TYPE]
-
-            atype = AttributeType.objects.filter(name=attribute_type)
-            if not atype:
-                logger.error('Attribute type with name %s does not exist. Skipping attribute.' % (attribute_type))
-                errors.append((subject_id, attribute_type, 'Attribute type does not exist.'))
-                current_count = update_count(current_count, task)
-                continue
-
-            # we can be pretty sure there is just one
-            atype = atype.first()
-            # get source content type (authority in this case)
-            ctype = ContentType.objects.filter(model=type(authority).__name__.lower()).first()
-
-            # content type of value
-            vctype = atype.value_content_type
-            avmodel_class = vctype.model_class()
-
-            att_init_values = {
-                'type_controlled': atype,
-                'source_content_type': ctype,
-                'source_instance_id': subject_id,
-                'value_freeform': row[COLUMN_NAME_ATTR_DATE_FREE],
-                'administrator_notes': row[COLUMN_NAME_ATTR_NOTES]
-            }
-
-            val_init_values = {}
-            if row[COLUMN_NAME_ATTR_VALUE]:
-                val_init_values.update({
-                    'value': row[COLUMN_NAME_ATTR_VALUE]
-                })
-
-            if row[COLUMN_NAME_ATTR_DATE_BEGIN]:
-                val_init_values.update({
-                    'start': ISODateValue.convert(row[COLUMN_NAME_ATTR_DATE_BEGIN])
-                })
-
-            if row[COLUMN_NAME_ATTR_DATE_END]:
-                val_init_values.update({
-                    'end': ISODateValue.convert(row[COLUMN_NAME_ATTR_DATE_END])
-                })
-
-            if row[COLUMN_NAME_ATTR_PLACE_NAME]:
-                val_init_values.update({
-                    'name': row[COLUMN_NAME_ATTR_PLACE_NAME]
-                })
-                att_init_values['value_freeform'] = row[COLUMN_NAME_ATTR_PLACE_NAME]
-
-            if row[COLUMN_NAME_ATTR_PLACE_LINK]:
+        try:
+            for row in csv.DictReader(f):
+                subject_id = row[COLUMN_NAME_ATTR_SUBJ_ID]
                 try:
-                    place = Authority.objects.get(pk=row[COLUMN_NAME_ATTR_PLACE_LINK])
-                    val_init_values.update({
-                        'value': place
-                    })
-                except:
-                    logger.error('Authority with id %s does not exist.' % (row[COLUMN_NAME_ATTR_PLACE_LINK]))
-                    errors.append((subject_id, row[COLUMN_NAME_ATTR_PLACE_LINK], 'Adding place link. Authority does not exist.'))
+                    authority = Authority.objects.get(pk=subject_id)
+                except Authority.DoesNotExist:
+                    logger.error('Authority with id %s does not exist. Skipping attribute.' % (subject_id))
+                    errors.append((subject_id, subject_id, 'Authority record does not exist.'))
                     current_count = update_count(current_count, task)
                     continue
 
-            attribute = Attribute(**att_init_values)
-            attribute.save()
+                related_name = row[COLUMN_NAME_ATTR_RELATED_NAME]
+                if authority.name != related_name:
+                    not_matching_subject_names.append((subject_id, authority.name, related_name))
 
-            val_init_values.update({
-                'attribute': attribute
-            })
+                attribute_type = row[COLUMN_NAME_ATTR_TYPE]
 
-            value = avmodel_class(**val_init_values)
-            value.save()
+                atype = AttributeType.objects.filter(name=attribute_type)
+                if not atype:
+                    logger.error('Attribute type with name %s does not exist. Skipping attribute.' % (attribute_type))
+                    errors.append((subject_id, attribute_type, 'Attribute type does not exist.'))
+                    current_count = update_count(current_count, task)
+                    continue
 
-            current_count = update_count(current_count, task)
+                # we can be pretty sure there is just one
+                atype = atype.first()
+                # get source content type (authority in this case)
+                ctype = ContentType.objects.filter(model=type(authority).__name__.lower()).first()
+
+                # content type of value
+                vctype = atype.value_content_type
+                avmodel_class = vctype.model_class()
+
+                att_init_values = {
+                    'type_controlled': atype,
+                    'source_content_type': ctype,
+                    'source_instance_id': subject_id,
+                    'value_freeform': row[COLUMN_NAME_ATTR_DATE_FREE],
+                    'administrator_notes': row[COLUMN_NAME_ATTR_NOTES]
+                }
+
+                val_init_values = {}
+                if row[COLUMN_NAME_ATTR_VALUE]:
+                    val_init_values.update({
+                        'value': row[COLUMN_NAME_ATTR_VALUE]
+                    })
+
+                if row[COLUMN_NAME_ATTR_DATE_BEGIN]:
+                    val_init_values.update({
+                        'start': ISODateValue.convert(row[COLUMN_NAME_ATTR_DATE_BEGIN])
+                    })
+
+                if row[COLUMN_NAME_ATTR_DATE_END]:
+                    val_init_values.update({
+                        'end': ISODateValue.convert(row[COLUMN_NAME_ATTR_DATE_END])
+                    })
+
+                if row[COLUMN_NAME_ATTR_PLACE_NAME]:
+                    val_init_values.update({
+                        'name': row[COLUMN_NAME_ATTR_PLACE_NAME]
+                    })
+                    att_init_values['value_freeform'] = row[COLUMN_NAME_ATTR_PLACE_NAME]
+
+                if row[COLUMN_NAME_ATTR_PLACE_LINK]:
+                    try:
+                        place = Authority.objects.get(pk=row[COLUMN_NAME_ATTR_PLACE_LINK])
+                        val_init_values.update({
+                            'value': place
+                        })
+                    except:
+                        logger.error('Authority with id %s does not exist.' % (row[COLUMN_NAME_ATTR_PLACE_LINK]))
+                        errors.append((subject_id, row[COLUMN_NAME_ATTR_PLACE_LINK], 'Adding place link. Authority does not exist.'))
+                        current_count = update_count(current_count, task)
+                        continue
+
+                attribute = Attribute(**att_init_values)
+                attribute.save()
+
+                val_init_values.update({
+                    'attribute': attribute
+                })
+
+                value = avmodel_class(**val_init_values)
+                value.save()
+
+                current_count = update_count(current_count, task)
+        except Exception, e:
+            logger.error("There was an unexpected error processing the CSV file.")
+            logger.exception(e)
+            errors.append(("unexpected error", "", "There was an unexpected error processing the CSV file: " + repr(e)))
 
         with smart_open.smart_open(error_path, 'wb') as f:
             writer = csv.writer(f)
