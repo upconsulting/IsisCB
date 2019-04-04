a7cef7c956243dfe89d87bb9b263d68c8baa24fc
Julia Damerow <julia.damerow@gmail.com>
2019-04-03 18:39:06 -0700

    [ISISCB-1161] added link check icon

--- isiscb/isisdata/templatetags/addcss.py
+++ isiscb/isisdata/templatetags/addcss.py
@@ -8,5 +8,8 @@ def addcss(field, css):
     parts = css.split(';')
     placeholder = parts[1] if len(parts) == 2 else ''
     css = parts[0]
-
+    other_prop = parts[2] if len(parts) == 3 else ''
+    other_prop_list = other_prop.split(":") if other_prop else None
+    if other_prop_list:
+        return field.as_widget(attrs={"class": css, "placeholder": placeholder, other_prop_list[0]: other_prop_list[1]})
     return field.as_widget(attrs={"class": css, "placeholder": placeholder})
