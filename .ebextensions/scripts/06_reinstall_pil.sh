ee16b5c35c82791aa47a8bb7ed3174bb9f60deb7
Julia Damerow <julia.damerow@gmail.com>
2020-01-04 16:02:28 -0500

    changed log message

--- .ebextensions/scripts/06_reinstall_pil.sh
+++ .ebextensions/scripts/06_reinstall_pil.sh
@@ -3,7 +3,7 @@
 echo 'This is a docker environment: $IS_DOCKER'
 
 if [ "$IS_DOCKER" != "true" ]; then
-  echo 'Reinstalling pillow...'
+  echo 'Reinstalling pillow version 6.2.1...'
   source /opt/python/run/venv/bin/activate
   yes | pip install Pillow==6.2.1 --no-cache-dir
 else
