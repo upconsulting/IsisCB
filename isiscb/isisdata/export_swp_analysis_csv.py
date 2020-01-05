570ec201e6877d536b9d92880928ac12d7677177
Julia Damerow <julia.damerow@gmail.com>
2020-01-05 17:33:10 -0500

    [IEXP-144] author editors names column

--- isiscb/isisdata/export_swp_analysis_csv.py
+++ isiscb/isisdata/export_swp_analysis_csv.py
@@ -13,12 +13,20 @@ import export_item_count_csv
 from django.conf import settings
 
 
+def _authors_editors_names(obj, extra, config={}):
+    fields = ['authority__id', 'authority__name', 'type_controlled']
+    names = obj.acrelation_set.filter(type_controlled__in=[ACRelation.EDITOR, ACRelation.AUTHOR])\
+                                   .order_by('data_display_order')\
+                                   .values_list(*fields)
+    return u' // '.join(map(lambda x: "AuthorityID %s || AuthorityName %s || Role %s"%(x[0], x[1], dict(ACRelation.TYPE_CHOICES)[x[2]]), names))
+
 
 object_id = export_item_count_csv.Column(u'Record number', lambda obj, extra, config={}: obj.id)
 print_status = export_item_count_csv.Column(u'Print status', export_item_count_csv._print_status)
 record_status = export_item_count_csv.Column(u'Record Status', lambda obj, extra, config={}: obj.get_record_status_value_display())
 citation_title = export_item_count_csv.Column(u'Title', export_item_count_csv._citation_title, Citation)
 record_type = export_item_count_csv.Column('Record Type', export_item_count_csv._record_type)
+authors_editors_names = export_item_count_csv.Column('Author/Editor Names', _authors_editors_names)
 
 tracking_records = export_item_count_csv.Column('Tracking Records', export_item_count_csv._tracking_records)
 record_action = export_item_count_csv.Column(u'Record Action', lambda obj, extra, config={}: obj.get_record_action_display())
@@ -36,6 +44,7 @@ CITATION_COLUMNS = [
     record_status,
     citation_title,
     record_type,
+    authors_editors_names,
 
     tracking_records,
     record_action,
