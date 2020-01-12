8fa1286dd98c2e71ec336bb9a147036c07806418
Julia Damerow <julia.damerow@gmail.com>
2020-01-12 14:19:32 -0500

    [IEXP-140, IEXP-144] added missing columns

--- isiscb/isisdata/export_swp_analysis_csv.py
+++ isiscb/isisdata/export_swp_analysis_csv.py
@@ -20,6 +20,65 @@ def _authors_editors_names(obj, extra, config={}):
                                    .values_list(*fields)
     return u' // '.join(map(lambda x: "AuthorityID %s || AuthorityName %s || Role %s"%(x[0], x[1], dict(ACRelation.TYPE_CHOICES)[x[2]]), names))
 
+def _publisher_school(obj, extra, config={}):
+    if obj.type_controlled in [Citation.BOOK, Citation.CHAPTER]:
+        obj_with_publisher = obj
+        # if we have a chapter we need to get the connect book
+        if obj.type_controlled == Citation.CHAPTER:
+            book_ccr = obj.ccrelations.filter(type_controlled__in=[CCRelation.INCLUDES_CHAPTER])
+            if book_ccr and book_ccr.first():
+                obj = book_ccr.first().subject
+
+        # get publisher
+        fields = ['authority__id', 'authority__name']
+        names = obj.acrelation_set.filter(type_controlled=ACRelation.PUBLISHER)\
+                                       .values_list(*fields)
+
+        return u' // '.join(map(lambda x: "AuthorityID %s || AuthorityName %s"%(x[0], x[1]), names))
+
+    # school missing
+
+def _journal_name(obj, extra, config={}):
+    qs = obj.acrelation_set.filter(type_controlled=ACRelation.PERIODICAL)
+    if qs.count() == 0:
+        return u""
+    _first = qs.first()
+    if _first.authority:
+        return unicode(_first.authority.name)
+    return u""
+
+def _volume(obj, extra, config={}):
+    if not hasattr(obj, 'part_details') or obj.part_details is None:
+        return u""
+    # ISISCB-1033
+    if obj.part_details.volume_free_text and obj.part_details.volume_free_text.strip():
+        return obj.part_details.volume_free_text.strip()
+
+    if obj.part_details.volume_begin or obj.part_details.volume_end:
+        return "-".join(map(lambda x: str(x), filter(None, [obj.part_details.volume_begin, obj.part_details.volume_end])))
+
+    return ''
+
+def _pages_free_text(obj, extra, config={}):
+    if not getattr(obj, 'part_details', None):
+        return u""
+    if obj.part_details.pages_free_text and obj.part_details.pages_free_text.strip():
+        return obj.part_details.pages_free_text.strip()
+
+    if obj.part_details.page_begin or obj.part_details.page_end:
+        return "-".join(map(lambda x: str(x), filter(None, [obj.part_details.page_begin, obj.part_details.page_end])))
+
+    return ''
+
+def _category(obj, extra, config={}):
+    fields = ['authority__name']
+    names = obj.acrelation_set.filter(type_controlled=ACRelation.CATEGORY)\
+                                   .values_list(*fields)
+    return u' || '.join(map(lambda x: x[0], names))
+
+def _language(obj, extra, config={}):
+    return u' || '.join(filter(lambda o: o is not None, list(obj.language.all().values_list('name', flat=True))))
+
 
 object_id = export_item_count_csv.Column(u'Record number', lambda obj, extra, config={}: obj.id)
 print_status = export_item_count_csv.Column(u'Print status', export_item_count_csv._print_status)
@@ -27,7 +86,13 @@ record_status = export_item_count_csv.Column(u'Record Status', lambda obj, extra
 citation_title = export_item_count_csv.Column(u'Title', export_item_count_csv._citation_title, Citation)
 record_type = export_item_count_csv.Column('Record Type', export_item_count_csv._record_type)
 authors_editors_names = export_item_count_csv.Column('Author/Editor Names', _authors_editors_names)
-
+publisher_school = export_item_count_csv.Column('Publisher/School', _publisher_school)
+journal_name = export_item_count_csv.Column("Journal Name", _journal_name)
+year_of_publication = export_item_count_csv.Column(u'Year Published', lambda obj, extra, config={}: obj.publication_date.year)
+volume = export_item_count_csv.Column(u"Volume", _volume)
+pages_free_text = export_item_count_csv.Column(u"Pages", _pages_free_text)
+category = export_item_count_csv.Column(u"Category", _category)
+language = export_item_count_csv.Column(u"Language", _language)
 tracking_records = export_item_count_csv.Column('Tracking Records', export_item_count_csv._tracking_records)
 record_action = export_item_count_csv.Column(u'Record Action', lambda obj, extra, config={}: obj.get_record_action_display())
 related_citations = export_item_count_csv.Column('Related Citations', export_item_count_csv._related_citations)
@@ -45,7 +110,13 @@ CITATION_COLUMNS = [
     citation_title,
     record_type,
     authors_editors_names,
-
+    publisher_school,
+    journal_name,
+    year_of_publication,
+    volume,
+    pages_free_text,
+    category,
+    language,
     tracking_records,
     record_action,
     related_citations,
