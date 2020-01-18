0d3a5cb75a04229bc21fd53b0c6e33aa045c3acd
Julia Damerow <julia.damerow@gmail.com>
2020-01-18 14:49:05 -0500

    [IEXP-145]  added changes up to Year Check

--- isiscb/isisdata/export_swp_analysis_csv.py
+++ isiscb/isisdata/export_swp_analysis_csv.py
@@ -36,7 +36,15 @@ def _publisher_school(obj, extra, config={}):
 
         return u' // '.join(map(lambda x: "AuthorityID %s || AuthorityName %s"%(x[0], x[1]), names))
 
-    # school missing
+    # school
+    if obj.type_controlled in [Citation.THESIS]:
+        fields = ['authority__id', 'authority__name']
+        names = obj.acrelation_set.filter(type_controlled=ACRelation.SCHOOL)\
+                                       .values_list(*fields)
+
+        return u' // '.join(map(lambda x: "AuthorityID %s || AuthorityName %s"%(x[0], x[1]), names))
+
+    return ""
 
 def _journal_name(obj, extra, config={}):
     qs = obj.acrelation_set.filter(type_controlled=ACRelation.PERIODICAL)
@@ -79,12 +87,94 @@ def _category(obj, extra, config={}):
 def _language(obj, extra, config={}):
     return u' || '.join(filter(lambda o: o is not None, list(obj.language.all().values_list('name', flat=True))))
 
+# check functions
+CHECK_WELL_FORMED = "Well-formed"
+CHECK_EMPTY = "Empty"
+CHECK_NON_STANDARD = "Non-Standard"
+CHECK_BROKEN_LINK = "BrokenLink"
+
+
+def _title_check(obj, extra, config={}):
+    if not obj.title and not obj.title.strip():
+        return CHECK_EMPTY
+
+    return CHECK_WELL_FORMED
+
+def _author_check(obj, extra, config={}):
+    authors = obj.acrelation_set.filter(type_controlled__in=[ACRelation.EDITOR, ACRelation.AUTHOR])
+    if not authors:
+        return CHECK_EMPTY
+
+    return _check_acrs(authors, [Authority.PERSON])
+
+def _publisher_school_check(obj, extra, config={}):
+    acrelations = None
+    if obj.type_controlled in [Citation.BOOK, Citation.CHAPTER]:
+        obj_with_publisher = obj
+        # if we have a chapter we need to get the connect book
+        if obj.type_controlled == Citation.CHAPTER:
+            book_ccr = obj.ccrelations.filter(type_controlled__in=[CCRelation.INCLUDES_CHAPTER])
+            if book_ccr and book_ccr.first():
+                obj = book_ccr.first().subject
+
+        # get publisher
+        acrelations = obj.acrelation_set.filter(type_controlled=ACRelation.PUBLISHER)
+
+    if obj.type_controlled in [Citation.THESIS]:
+        acrelations = obj.acrelation_set.filter(type_controlled=ACRelation.SCHOOL)
+
+    if not acrelations:
+        return CHECK_EMPTY
+
+    return _check_acrs(acrelations, [Authority.INSTITUTION])
+
+def _journal_check(obj, extra, config={}):
+    journals = obj.acrelation_set.filter(type_controlled=ACRelation.PERIODICAL)
+    if not journals:
+        return CHECK_EMPTY
+
+    return _check_acrs(journals, [Authority.SERIAL_PUBLICATION])
+
+def _year_check(obj, extra, config={}):
+    year = obj.publication_date.year
+    if not year:
+        return CHECK_EMPTY
+
+    if year < 1970:
+        return CHECK_NON_STANDARD
+
+    return CHECK_WELL_FORMED
+
+def _check_acrs(acrs, authority_types):
+    is_non_standard = False
+    for acr in acrs:
+        if is_broken_link(acr):
+            return CHECK_BROKEN_LINK
+
+        if acr.authority.type_controlled not in authority_types:
+            is_non_standard = True
+
+    if is_non_standard:
+        return CHECK_NON_STANDARD
+
+    return CHECK_WELL_FORMED
+
+def is_broken_link(acr):
+    return acr.record_status_value != CuratedMixin.ACTIVE or not acr.authority or acr.authority.record_status_value != CuratedMixin.ACTIVE
 
 object_id = export_item_count_csv.Column(u'Record number', lambda obj, extra, config={}: obj.id)
 print_status = export_item_count_csv.Column(u'Print status', export_item_count_csv._print_status)
 record_status = export_item_count_csv.Column(u'Record Status', lambda obj, extra, config={}: obj.get_record_status_value_display())
-citation_title = export_item_count_csv.Column(u'Title', export_item_count_csv._citation_title, Citation)
+curation_link = export_item_count_csv.Column('Curation Link', export_item_count_csv._curation_link)
+public_link = export_item_count_csv.Column('Public Link', export_item_count_csv._public_link)
 record_type = export_item_count_csv.Column('Record Type', export_item_count_csv._record_type)
+title_check = export_item_count_csv.Column('Title Check', _title_check)
+author_check = export_item_count_csv.Column('Auth/Ed Check', _author_check)
+publisher_school_check = export_item_count_csv.Column('Pub/Sch Check', _publisher_school_check)
+journal_check = export_item_count_csv.Column('Jrnl Check', _journal_check)
+year_check = export_item_count_csv.Column('Year Check', _year_check)
+
+citation_title = export_item_count_csv.Column(u'Title', export_item_count_csv._citation_title, Citation)
 authors_editors_names = export_item_count_csv.Column('Author/Editor Names', _authors_editors_names)
 publisher_school = export_item_count_csv.Column('Publisher/School', _publisher_school)
 journal_name = export_item_count_csv.Column("Journal Name", _journal_name)
@@ -107,8 +197,16 @@ CITATION_COLUMNS = [
     object_id,
     print_status,
     record_status,
-    citation_title,
+    curation_link,
+    public_link,
     record_type,
+    title_check,
+    author_check,
+    publisher_school_check,
+    journal_check,
+    year_check,
+
+    citation_title,
     authors_editors_names,
     publisher_school,
     journal_name,
