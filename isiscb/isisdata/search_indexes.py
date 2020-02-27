59b2b1f10f817e9051d615d146ebe5a139bce4a9
Julia Damerow <julia.damerow@gmail.com>
2020-02-26 21:43:00 -0500

    [IEXP-156, IEXP-21] changed indexing for layout and search changes

--- isiscb/isisdata/search_indexes.py
+++ isiscb/isisdata/search_indexes.py
@@ -103,6 +103,11 @@ class CitationIndex(indexes.SearchIndex, indexes.Indexable):
     creative_works = indexes.MultiValueField(faceted=True, indexed=False)
     events = indexes.MultiValueField(faceted=True, indexed=False)
 
+    # IEXP-21: for facet boxes on authority page
+    concepts_by_subject_ids = indexes.MultiValueField(faceted=True, indexed=False, null=True)
+    people_by_subject_ids = indexes.MultiValueField(faceted=True, indexed=False, null=True)
+    institutions_by_subject_ids = indexes.MultiValueField(faceted=True, indexed=False, null=True)
+
     data_fields = [
         'id',
         'title',
@@ -262,22 +267,24 @@ class CitationIndex(indexes.SearchIndex, indexes.Indexable):
                     multivalue_data['geographics'].append(name)
                     multivalue_data['geographic_ids'].append(ident)
                 else:
-
-
                     if a['acrelation__authority__type_controlled'] == Authority.INSTITUTION:
                         multivalue_data['subject_institutions'].append(name)
+                        multivalue_data['institutions_by_subject_ids'].append(ident)
                     elif a['acrelation__authority__type_controlled'] == Authority.PERSON:
                         multivalue_data['people'].append(name)
+                        multivalue_data['people_by_subject_ids'].append(ident)
                     elif a['acrelation__authority__type_controlled']  == Authority.SERIAL_PUBLICATION:
                         multivalue_data['serial_publications'].append(name)
                     elif a['acrelation__authority__type_controlled']  == Authority.CLASSIFICATION_TERM:
                         multivalue_data['classification_terms'].append(name)
                     elif a['acrelation__authority__type_controlled']  == Authority.CONCEPT:
                         multivalue_data['concepts'].append(name)
+                        multivalue_data['concepts_by_subject_ids'].append(ident)
                     elif a['acrelation__authority__type_controlled']  == Authority.CREATIVE_WORK:
                         multivalue_data['creative_works'].append(name)
                     elif a['acrelation__authority__type_controlled']  == Authority.EVENT:
                         multivalue_data['events'].append(name)
+
 
             elif a['acrelation__type_controlled'] == ACRelation.INSTITUTION:
                 multivalue_data['institutions'].append(name)
@@ -292,11 +299,15 @@ class CitationIndex(indexes.SearchIndex, indexes.Indexable):
                 multivalue_data['translators'].append(name)
                 multivalue_data['translator_ids'].append(ident)
             elif a['acrelation__type_controlled'] == ACRelation.PUBLISHER:
-                multivalue_data['publishers'].append(name)
-                multivalue_data['publisher_ids'].append(ident)
+                if name not in multivalue_data['publishers']:
+                    multivalue_data['publishers'].append(name)
+                if ident not in multivalue_data['publisher_ids']:
+                    multivalue_data['publisher_ids'].append(ident)
             elif a['acrelation__type_controlled'] == ACRelation.SCHOOL:
-                multivalue_data['schools'].append(name)
-                multivalue_data['school_ids'].append(ident)
+                if name not in multivalue_data['schools']:
+                    multivalue_data['schools'].append(name)
+                if ident not in multivalue_data['school_ids']:
+                    multivalue_data['school_ids'].append(ident)
             elif a['acrelation__type_controlled'] == ACRelation.MEETING:
                 multivalue_data['meetings'].append(name)
                 multivalue_data['meeting_ids'].append(ident)
@@ -315,7 +326,8 @@ class CitationIndex(indexes.SearchIndex, indexes.Indexable):
 
             elif a['acrelation__type_broad_controlled'] == ACRelation.SUBJECT_CONTENT:
                 multivalue_data['about_person_ids'].append(ident)
-            elif a['acrelation__type_broad_controlled'] in [ACRelation.INSTITUTIONAL_HOST, ACRelation.PUBLICATION_HOST, ACRelation.PERSONAL_RESPONS] and a['acrelation__type_controlled'] not in [ACRelation.AUTHOR, ACRelation.CONTRIBUTOR]:
+
+            if a['acrelation__type_broad_controlled'] in [ACRelation.INSTITUTIONAL_HOST, ACRelation.PUBLICATION_HOST, ACRelation.PERSONAL_RESPONS] and a['acrelation__type_controlled'] not in [ACRelation.AUTHOR, ACRelation.CONTRIBUTOR]:
                 multivalue_data['other_person_ids'].append(ident)
 
             if a['acrelation__type_broad_controlled'] == ACRelation.PERSONAL_RESPONS:
@@ -328,11 +340,13 @@ class CitationIndex(indexes.SearchIndex, indexes.Indexable):
 
                 # Prefer the ACRelation.name_for_display_in_citation, if
                 #  present.
-                aname = a['acrelation__name_for_display_in_citation']
-                if not aname:
-                    aname = name
-                if aname not in multivalue_data['authors']:
-                    multivalue_data['authors'].append(aname)
+                # IEXP-31: only append authors and editors to authors field
+                if a['acrelation__type_controlled'] in [ACRelation.AUTHOR, ACRelation.EDITOR]:
+                    aname = a['acrelation__name_for_display_in_citation']
+                    if not aname:
+                        aname = name
+                    if aname not in multivalue_data['authors']:
+                        multivalue_data['authors'].append(aname)
 
         if len(multivalue_data['authors']) > 0:
             self.prepared_data['author_for_sort'] = multivalue_data['authors'][0]
@@ -498,7 +512,7 @@ class CitationIndex(indexes.SearchIndex, indexes.Indexable):
         """
         This is here just so that we can sort chapters in books.
         """
-        if data['type_controlled'] != Citation.CHAPTER:
+        if data['type_controlled'] not in [Citation.CHAPTER, Citation.ARTICLE]:
             return ""
         page_start_string = data['part_details']['page_begin']
         page_end_string = data['part_details']['page_end']
