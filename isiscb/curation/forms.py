356c8ab0bc085014049813c8227ac028067c48ee
Julia Damerow <julia.damerow@gmail.com>
2021-09-19 20:43:29 -0400

    [IEXP-302] moved preset export option to dropdown

--- isiscb/curation/forms.py
+++ isiscb/curation/forms.py
@@ -835,11 +835,15 @@ class SelectAuthorityCollectionForm(forms.Form):
 
 class ExportCitationsForm(forms.Form):
     export_name = forms.CharField(help_text='This tag will be added to the export filename')
-    export_format = forms.ChoiceField(choices=[('CSV', 'Comma-separated values (CSV)'), ('EBSCO_CSV', 'Comma-separated values (CSV) in EBSCO format (disregard column selection below)'), ('ITEM_COUNT', 'Export for Item Counts'), ('SWP_ANALYSIS', "Export for SPW Analysis")])
+    export_format = forms.ChoiceField(choices=[
+        ('CSV', 'Comma-separated values (CSV)'),
+        ('EBSCO_CSV', 'Comma-separated values (CSV) in EBSCO format (disregard column selection below)'),
+        ('ITEM_COUNT', 'Export for Item Counts'),
+        ('SWP_ANALYSIS', "Export for SPW Analysis"),
+        ('SWP_PRESET', "Export for regular data backup (disregard column selection below).")])
     export_linked_records = forms.BooleanField(label="Export linked records (make sure that the 'Link to Record' Field is selected in the field list)", required=False)
     export_metadata = forms.BooleanField(label="Export metadata", required=False)
     use_pipe_delimiter = forms.BooleanField(label='Use "||" to separate related authority and citation fields', required=False)
-    use_preset = forms.BooleanField(label='Export predefined fields only (disregard column selection below).', required=False)
     fields = forms.MultipleChoiceField(choices=[(c.slug, c.label) for c in export.CITATION_COLUMNS], required=False)
     filters = forms.CharField(widget=forms.widgets.HiddenInput())
     # compress_output = forms.BooleanField(required=False, initial=True,
@@ -849,9 +853,8 @@ class ExportCitationsForm(forms.Form):
     def clean_fields(self):
         field_data = self.cleaned_data['fields']
         export_type = self.cleaned_data['export_format']
-        use_preset = self.cleaned_data['use_preset']
         if export_type == 'CSV':
-            if not field_data and not use_preset:
+            if not field_data:
                 raise forms.ValidationError("Please select fields to export.")
 
         return field_data
