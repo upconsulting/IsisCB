b5a11b8b0ca230d88ac55d05ae75ec9f17d6292a
Paul Kelley Vieth <pvieth@ou.edu>
2022-01-07 10:01:46 -0600

    fixing typos, moving wikipedia data refresh rate to settings

--- isiscb/isiscb/production_settings.py
+++ isiscb/isiscb/production_settings.py
@@ -433,3 +433,6 @@ DATASET_SHOT_NAME_DISPLAY = os.environ.get('DATASET_SHOT_NAME_DISPLAY', 'Technol
 WIKIPEDIA_IMAGE_API_PATH = os.environ.get('WIKIPEDIA_IMAGE_API_PATH', 'https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles={authorityName}&origin=*')
 WIKIPEDIA_INTRO_API_PATH = os.environ.get('WIKIPEDIA_INTRO_API_PATH', 'https://en.wikipedia.org/w/api.php?action=query&titles={authorityName}&prop=extracts&exintro&explaintext&redirects=1&format=json&origin=*')
 WIKIPEDIA_PAGE_PATH = os.environ.get('WIKIPEDIA_PAGE_PATH', 'https://en.wikipedia.org/?curid=')
+
+# number of days after which we want to refresh cached data about authorities from wikipedia
+WIKIPEDIA_REFRESH_TIME = os.environ.get('WIKPEDIA_REFRESH_TIME', 30)
--- isiscb/isisdata/isiscbviews/authority_views.py
+++ isiscb/isisdata/isiscbviews/authority_views.py
@@ -329,56 +329,55 @@ def authority(request, authority_id):
     return render(request, 'isisdata/authority.html', context)
 
 def get_wikipedia_image_synopsis(authority, author_contributor_count, related_citations_count):
-    wikiImage = ''
-    wikiCredit = ''
-    wikiIntro = ''
-    authorityName = ''
+    if not authority.type_controlled == authority.SERIAL_PUBLICATION and not(authority.type_controlled == authority.PERSON and author_contributor_count != 0 and author_contributor_count/related_citations_count > .9):
+        wikiImage = ''
+        wikiCredit = ''
+        wikiIntro = ''
 
-    wikipedia_data = WikipediaData.objects.filter(authority__id=authority.id).first()
+        wikipedia_data = WikipediaData.objects.filter(authority__id=authority.id).first()
 
-    if wikipedia_data and (datetime.datetime.now(datetime.timezone.utc) - wikipedia_data.last_modified).days < 30 and wikipedia_data.img_url and wikipedia_data.intro and wikipedia_data.credit :
-        wikiImage = wikipedia_data.img_url
-        wikiCredit = wikipedia_data.credit
-        wikiIntro = wikipedia_data.intro
+        if wikipedia_data and (datetime.datetime.now(datetime.timezone.utc) - wikipedia_data.last_modified).days < settings.WIKIPEDIA_REFRESH_TIME:
+            wikiImage = wikipedia_data.img_url
+            wikiCredit = wikipedia_data.credit
+            wikiIntro = wikipedia_data.intro
 
-    else:
-        if not authority.type_controlled == authority.SERIAL_PUBLICATION and not(authority.type_controlled == authority.PERSON and author_contributor_count != 0 and author_contributor_count/related_citations_count > .9):
+        else:
             authorityName = authority.name
             if hasattr(authority, 'person'):
                 if authorityName.find(',') >= 0:
-                    firstName = authorityName[authorityName.index(',') + 2:len(authorityName)].strip()
+                    firstName = authorityName[authorityName.index(',')+1:len(authorityName)].strip()
                     if firstName.find(',') >= 0:
-                        firstName = firstName[:firstName.find(',')]
-                    lastName = authorityName[:authorityName.find(',')]
-                    authorityName = firstName + ' ' + lastName  
-            elif authority.type_controlled == authority.PERSON:
+                        firstName = firstName[:firstName.find(',')].strip()
+                    lastName = authorityName[:authorityName.find(',')].strip()
+                    authorityName = firstName + ' ' + lastName
+            elif authority.type_controlled == authority.CONCEPT:
                 if authorityName.find(';') >= 0:
-                    authorityName = authorityName[:authorityName.find(';').strip()]
+                    authorityName = authorityName[:authorityName.find(';')].strip()
             elif authority.type_controlled == authority.GEOGRAPHIC_TERM:
                 if authorityName.find('(') >= 0:
-                    authorityName = authorityName[:authorityName.find('(').strip]
+                    authorityName = authorityName[:authorityName.find('(')].strip()
 
-        if authorityName:
-            imgURL = settings.WIKIPEDIA_IMAGE_API_PATH.format(authorityName = authorityName)
-            introURL = settings.WIKIPEDIA_INTRO_API_PATH.format(authorityName = authorityName)
-            imgJSON = requests.get(imgURL).json()
+            if authorityName:
+                imgURL = settings.WIKIPEDIA_IMAGE_API_PATH.format(authorityName = authorityName)
+                introURL = settings.WIKIPEDIA_INTRO_API_PATH.format(authorityName = authorityName)
+                imgJSON = requests.get(imgURL).json()
 
-            if list(imgJSON['query']['pages'].items())[0][0] != '-1':
-                imgPage = list(imgJSON['query']['pages'].items())[0][1]
-                imgPageID = imgPage['pageid']
-                wikiCredit = f'{settings.WIKIPEDIA_PAGE_PATH}{imgPageID}'
-                if imgPage.get('original') and imgPage['original'].get('source'):
-                    wikiImage = imgPage['original']['source']
+                if list(imgJSON['query']['pages'].items())[0][0] != '-1':
+                    imgPage = list(imgJSON['query']['pages'].items())[0][1]
+                    imgPageID = imgPage['pageid']
+                    wikiCredit = f'{settings.WIKIPEDIA_PAGE_PATH}{imgPageID}'
+                    if imgPage.get('original') and imgPage['original'].get('source'):
+                        wikiImage = imgPage['original']['source']
 
-                introJSON = requests.get(introURL).json()
-                extract = list(introJSON['query']['pages'].items())[0][1]['extract']
-                if extract.find('may refer to') < 0:
-                    wikiIntro = extract
-        
-        wikipedia_data = WikipediaData(img_url=wikiImage, credit=wikiCredit, intro=wikiIntro, authority_id=authority.id)
-        wikipedia_data.save()
+                    introJSON = requests.get(introURL).json()
+                    extract = list(introJSON['query']['pages'].items())[0][1]['extract']
+                    if extract.find('may refer to') < 0:
+                        wikiIntro = extract
+            
+            wikipedia_data = WikipediaData(img_url=wikiImage, credit=wikiCredit, intro=wikiIntro, authority_id=authority.id)
+            wikipedia_data.save()
 
-    return wikiImage, wikiIntro, wikiCredit
+        return wikiImage, wikiIntro, wikiCredit
 
 def get_place_map_data(request, authority_id):
     sqs =SearchQuerySet().models(Citation).facet('geographic_ids', size=1000).facet('geocodes', size=1000)
