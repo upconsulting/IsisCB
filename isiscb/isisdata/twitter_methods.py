ae9b470038fd0209c7f95504ca4c6fe293fefbbf
crispzips <pvieth@ou.edu>
2022-04-02 08:50:42 -0400

    quality control

--- isiscb/isisdata/twitter_methods.py
+++ isiscb/isisdata/twitter_methods.py
@@ -10,15 +10,14 @@ def get_five_most_recent_tweets():
     with requests.get(recent_tweets_url, headers={"Authorization": f"Bearer {twitter_bearer_token}"}) as resp:
         if resp.status_code != 200:
             return []
-        else:
-            return resp.json()
+        return resp.json()
 
 def get_featured_tweet():
     recent_tweet_image, recent_tweet_text, recent_tweet_url = ''
 
     recent_tweets = get_five_most_recent_tweets()
 
-    recent_tweet_id = recent_tweets['data'][random.randint(0,len(recent_tweets['data'])-1)]['id'] if 'data' in recent_tweets else ''
+    recent_tweet_id = recent_tweets['data'][random.randint(0,len(recent_tweets['data'])-1)]['id'] if 'data' in recent_tweets and len(recent_tweets)['data'] > 0 else ''
 
     if recent_tweet_id:
         with requests.get(tweet_url.format(tweetID=recent_tweet_id), headers={"Authorization": f"Bearer {twitter_bearer_token}"}) as resp:
@@ -28,8 +27,8 @@ def get_featured_tweet():
                 recent_tweet_url = recent_tweet_text[recent_tweet_text.rfind('https://'):]
 
                 recent_tweet_text = recent_tweet_text[:recent_tweet_text.rfind('https://')]
-                URL_REGEX = re.compile(r'''((?:https://).{15})''')
-                recent_tweet_text = URL_REGEX.sub(r'<a href="\1">\1</a>', recent_tweet_text)
+                url_regex = re.compile(r'''((?:https://).{15})''')
+                recent_tweet_text = url_regex.sub(r'<a href="\1">\1</a>', recent_tweet_text)
 
                 if 'includes' in recent_tweet and recent_tweet['includes'] and recent_tweet['includes']['media'] and recent_tweet['includes']['media'][0]:
                     recent_tweet_image = recent_tweet['includes']['media'][0]['url']
