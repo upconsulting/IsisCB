517ba23f061637bbf639e5c0a6156d562061015a
Julia Damerow <julia.damerow@gmail.com>
2023-06-10 21:35:39 -0400

    [IEXP-403] remove featured theme box, remove empty tweet box

---
+++ isiscb/tenants/templatetags/tenanttags.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+from django import template
+register = template.Library()
+
+@register.filter
+def featured_column_width(tweet_url):
+    print("url", tweet_url)
+    if tweet_url:
+        return 8
+    return 12
