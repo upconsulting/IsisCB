05b4863e5cffe8bfe72a866029101c904baec3d7
Julia Damerow <julia.damerow@gmail.com>
2023-09-19 21:33:22 -0400

    [IEXP-460] non-public citations are marked when searching; home page doesn't who non-public citation

--- isiscb/tenants/templatetags/tenanttags.py
+++ isiscb/tenants/templatetags/tenanttags.py
@@ -7,3 +7,7 @@ def featured_column_width(tweet_url):
     if tweet_url:
         return 8
     return 12
+
+@register.filter
+def get_featured_column_width(citation, authority):
+    return '12' if not citation.public or not authority.public else '6'
