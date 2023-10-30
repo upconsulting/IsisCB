bb1cc4191d647e9b1fd4b7dcde132741cf3ce672
Julia Damerow <julia.damerow@gmail.com>
2023-10-29 21:02:27 -0400

    [IEXP-469] fixed search links to include/exclude all project in search according to general setting

--- isiscb/tenants/context_processors.py
+++ isiscb/tenants/context_processors.py
@@ -2,7 +2,15 @@ from isisdata.models import Tenant
 from django.shortcuts import get_object_or_404
 
 def add_tenants(request):
+    context = {}
     if 'tenant_id' in request.resolver_match.kwargs:
         tenant_id = request.resolver_match.kwargs['tenant_id']
-        return {'tenant_id': tenant_id , 'tenant': get_object_or_404(Tenant, identifier=tenant_id) if tenant_id else None}
-    return {}
+        context.update({
+            'tenant_id': tenant_id , 
+            'tenant': get_object_or_404(Tenant, identifier=tenant_id) if tenant_id else None
+        }) 
+    context.update({
+        'include_all_tenants': request.include_all_tenants
+    })
+
+    return context
