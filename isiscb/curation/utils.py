3f6c3c41bb2cd20dcc28b53fa626ae38bde42c18
crispzips <pvieth@ou.edu>
2024-02-01 11:46:04 -0500

    [IEXP-488] made html snippeting functionality more readable and better documented

--- isiscb/curation/utils.py
+++ isiscb/curation/utils.py
@@ -34,22 +34,25 @@ def get_printlike_citation(id):
 
 # formats list of contributor names depending on the number of contributors in the last; formats all surnames in smallcaps
 def _format_contributors(citation): 
+    """
+     Function to format the list of contributors, in the following way:
+     - one author: LASTNAME, Firstname.
+     - two authors: LASTNAME1, Firstname1 and Firstname2 LASTNAME2.
+     - three authors: LASTNAME1, Firstname1, Firstname2 LASTNAME2, and Firstname3 LASTNAME3.
+     - more than three authors: LASTNAME1, Firstname1, Firstname2 LASTNAME2, Firstname3 LASTNAME3, et al.
+    """
     contrib_acrelations = citation.get_all_contributors
     contribs = [acrel.authority.name for acrel in filter(lambda rel: rel.authority.name, contrib_acrelations)]
 
     formatted_contributors = ''
     if contribs:
-        formatted_contributors = _build_name_last_first_html(contribs[0]) # if there's just 1 contributor: "LASTNAME, Firstname"
-    
+        formatted_contributors = _build_name_last_first_html(contribs[0])
         if len(contribs) == 1:
             formatted_contributors = formatted_contributors + '.'
-        # if 2 contributors: "LASTNAME1, Firstname1 and Firstname2 LASTNAME2."
         elif len(contribs) == 2:
             formatted_contributors = formatted_contributors + 'and ' + _build_name_first_last_html(contribs[1]) + '.'
-        # if 2 contributors: "LASTNAME1, Firstname1, Firstname2 LASTNAME2, and Firstname3 LASTNAME3."
         elif len(contribs) == 3:
             formatted_contributors = formatted_contributors + ', ' + _build_name_first_last_html(contribs[1]) + ', and ' + _build_name_first_last_html(contribs[2]) + '.'
-        # if more than 3 contributors: "LASTNAME1, Firstname1, Firstname2 LASTNAME2, Firstname3 LASTNAME3, et al."
         elif len(contribs) > 3:
             formatted_contributors = formatted_contributors + ', ' + _build_name_first_last_html(contribs[1]) + ', ' + _build_name_first_last_html(contribs[2]) + ', et al.'
     
@@ -79,39 +82,54 @@ def _format_title(citation):
     return '"' + title + '."'
 
 def _format_publisher_or_periodical(citation):
-    publishers = ACRelation.objects.filter(citation=citation, type_controlled=ACRelation.PUBLISHER).filter(public=True)
-    containing_citation = CCRelation.objects.filter(object_id=citation.id, type_controlled=CCRelation.INCLUDES_CHAPTER, subject__public=True).filter(public=True)
-    periodicals = ACRelation.objects.filter(citation=citation, type_controlled=ACRelation.PERIODICAL).filter(public=True)
+    """
+    Function to format the publisher (for books), container book (for chapters), or hosting periodical (for journal articles, essay reviews, book reviews)
+    - for books: "PublisherName,"
+    - for chapters: " In TitleOfBookContainingTheChapter, edited by 
+    """
     formatted_publisher_or_periodical = ''
+
+    publishers = ACRelation.objects.filter(citation=citation, type_controlled=ACRelation.PUBLISHER).filter(public=True)
     if citation.type_controlled == Citation.BOOK and publishers: 
         return publishers[0].authority.name + ','
+    
+    containing_citation = CCRelation.objects.filter(object_id=citation.id, type_controlled=CCRelation.INCLUDES_CHAPTER, subject__public=True).filter(public=True)
     if citation.type_controlled == Citation.CHAPTER and containing_citation:
         formatted_publisher_or_periodical = ' In <i>' + containing_citation[0].subject.title + '</i>'
         containing_citation_editor = containing_citation[0].subject.get_all_contributors[0].authority if containing_citation[0].subject.get_all_contributors else None
         if containing_citation_editor:
-            formatted_publisher_or_periodical = formatted_publisher_or_periodical + ', edited by <span style="font-variant: small-caps;">' + containing_citation_editor.name
+            formatted_publisher_or_periodical = formatted_publisher_or_periodical + ', edited by <span style="font-variant: small-caps;">' + containing_citation_editor.name + '</span>'
         return formatted_publisher_or_periodical
+    
     if citation.type_controlled == Citation.THESIS:
         schools = ACRelation.objects.filter(citation=citation.id, type_controlled=ACRelation.SCHOOL).filter(public=True)
-        if schools:
-            return 'Dissertation at ' + schools[0].authority.name
+        return 'Dissertation at ' + schools[0].authority.name if schools else ''
+    
+    periodicals = ACRelation.objects.filter(citation=citation, type_controlled=ACRelation.PERIODICAL).filter(public=True)
     if periodicals:
-        formatted_publisher_or_periodical = '<i>' + periodicals[0].authority.name + '</i> '
-        if citation.part_details.volume:
-            formatted_publisher_or_periodical = formatted_publisher_or_periodical + citation.part_details.volume
-        elif citation.part_details.volume_free_text:
-            formatted_publisher_or_periodical = formatted_publisher_or_periodical + citation.part_details.volume_free_text
-        elif citation.part_details.volume_begin:
-            formatted_publisher_or_periodical = formatted_publisher_or_periodical + str(citation.part_details.volume_begin)
-            if citation.part_details.volume_end:
-                formatted_publisher_or_periodical = formatted_publisher_or_periodical + '-' + str(citation.part_details.volume_end)
-        if citation.part_details.issue_free_text:
-            formatted_publisher_or_periodical = formatted_publisher_or_periodical + ', no. ' + citation.part_details.issue_free_text
-        elif citation.part_details.issue_begin:
-            formatted_publisher_or_periodical = formatted_publisher_or_periodical + ', no. ' + str(citation.part_details.issue_begin)
-            if citation.part_details.issue_end:
-                formatted_publisher_or_periodical = formatted_publisher_or_periodical + '-' + str(citation.part_details.issue_end)
-        return formatted_publisher_or_periodical
+        return _format_periodicals(citation, periodicals)
+
+def _format_periodicals(citation, periodicals):
+    """
+    formats volume number and issue numbers for periodicals, 
+    accounting for the fact that some records have data in multiple fields describing volume and issue numbers
+    """
+    formatted_publisher_or_periodical = '<i>' + periodicals[0].authority.name + '</i> '
+    if citation.part_details.volume:
+        formatted_publisher_or_periodical = formatted_publisher_or_periodical + citation.part_details.volume
+    elif citation.part_details.volume_free_text:
+        formatted_publisher_or_periodical = formatted_publisher_or_periodical + citation.part_details.volume_free_text
+    elif citation.part_details.volume_begin:
+        formatted_publisher_or_periodical = formatted_publisher_or_periodical + str(citation.part_details.volume_begin)
+        if citation.part_details.volume_end:
+            formatted_publisher_or_periodical = formatted_publisher_or_periodical + '-' + str(citation.part_details.volume_end)
+    if citation.part_details.issue_free_text:
+        formatted_publisher_or_periodical = formatted_publisher_or_periodical + ', no. ' + citation.part_details.issue_free_text
+    elif citation.part_details.issue_begin:
+        formatted_publisher_or_periodical = formatted_publisher_or_periodical + ', no. ' + str(citation.part_details.issue_begin)
+        if citation.part_details.issue_end:
+            formatted_publisher_or_periodical = formatted_publisher_or_periodical + '-' + str(citation.part_details.issue_end)
+    return formatted_publisher_or_periodical
 
 def _format_date(citation, formatted_publisher_or_periodical):
     date = citation.publication_date
