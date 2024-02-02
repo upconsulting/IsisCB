9229e06c726298e436872c6b34259865eba1cb9a
crispzips <pvieth@ou.edu>
2024-02-01 19:45:59 -0500

    [IEXP-488] checked for attributes everywhere because of bad data

--- isiscb/curation/utils.py
+++ isiscb/curation/utils.py
@@ -42,7 +42,11 @@ def _format_contributors(citation):
      - more than three authors: LASTNAME1, Firstname1, Firstname2 LASTNAME2, Firstname3 LASTNAME3, et al.
     """
     contrib_acrelations = citation.get_all_contributors
-    contribs = [acrel.authority.name for acrel in filter(lambda rel: rel.authority.name, contrib_acrelations)]
+    contribs = []
+    if contrib_acrelations:
+        for contrib_acrelation in contrib_acrelations:
+            if hasattr(contrib_acrelation.authority, "name"):
+                contribs.append(contrib_acrelation.authority.name)
 
     formatted_contributors = ''
     if contribs:
@@ -90,44 +94,42 @@ def _format_publisher_or_periodical(citation):
     formatted_publisher_or_periodical = ''
 
     publishers = ACRelation.objects.filter(citation=citation, type_controlled=ACRelation.PUBLISHER).filter(public=True)
-    if citation.type_controlled == Citation.BOOK and publishers: 
-        return publishers[0].authority.name + ','
+    if citation.type_controlled == Citation.BOOK: 
+        return publishers[0].authority.name + ',' if publishers and hasattr(publishers[0].authority,'name') else ''
     
     containing_citation = CCRelation.objects.filter(object_id=citation.id, type_controlled=CCRelation.INCLUDES_CHAPTER, subject__public=True).filter(public=True)
-    if citation.type_controlled == Citation.CHAPTER and containing_citation:
-        formatted_publisher_or_periodical = ' In <i>' + containing_citation[0].subject.title + '</i>'
-        containing_citation_editor = containing_citation[0].subject.get_all_contributors[0].authority if containing_citation[0].subject.get_all_contributors else None
-        if containing_citation_editor:
-            formatted_publisher_or_periodical = formatted_publisher_or_periodical + ', edited by <span style="font-variant: small-caps;">' + containing_citation_editor.name + '</span>'
+    if citation.type_controlled == Citation.CHAPTER:
+        formatted_publisher_or_periodical = ' In <i>' + containing_citation[0].subject.title + '</i>' if containing_citation else ''
+        containing_citation_editor = containing_citation[0].subject.get_all_contributors[0].authority if containing_citation and containing_citation[0].subject.get_all_contributors else None
+        formatted_publisher_or_periodical = formatted_publisher_or_periodical + (', edited by <span style="font-variant: small-caps;">' + containing_citation_editor.name + '</span>' if containing_citation_editor else '')
         return formatted_publisher_or_periodical
     
     if citation.type_controlled == Citation.THESIS:
         schools = ACRelation.objects.filter(citation=citation.id, type_controlled=ACRelation.SCHOOL).filter(public=True)
-        return 'Dissertation at ' + schools[0].authority.name if schools else ''
+        return 'Dissertation at ' + schools[0].authority.name if schools and hasattr(schools[0].authority,'name') else ''
     
     periodicals = ACRelation.objects.filter(citation=citation, type_controlled=ACRelation.PERIODICAL).filter(public=True)
-    if periodicals:
-        return _format_periodicals(citation, periodicals)
+    return _format_periodicals(citation, periodicals) if periodicals else ''
 
 def _format_periodicals(citation, periodicals):
     """
     formats volume number and issue numbers for periodicals, 
     accounting for the fact that some records have data in multiple fields describing volume and issue numbers
     """
-    formatted_publisher_or_periodical = '<i>' + periodicals[0].authority.name + '</i> '
-    if citation.part_details.volume:
+    formatted_publisher_or_periodical = '<i>' + periodicals[0].authority.name + '</i> ' if hasattr(periodicals[0].authority, 'name') else ''
+    if  hasattr(citation.part_details,'volume') and citation.part_details.volume:
         formatted_publisher_or_periodical = formatted_publisher_or_periodical + citation.part_details.volume
-    elif citation.part_details.volume_free_text:
+    elif  hasattr(citation.part_details,'volume_free_text') and citation.part_details.volume_free_text:
         formatted_publisher_or_periodical = formatted_publisher_or_periodical + citation.part_details.volume_free_text
-    elif citation.part_details.volume_begin:
+    elif hasattr(citation.part_details,'volume_begin') and citation.part_details.volume_begin:
         formatted_publisher_or_periodical = formatted_publisher_or_periodical + str(citation.part_details.volume_begin)
-        if citation.part_details.volume_end:
+        if hasattr(citation.part_details,'volume_end') and citation.part_details.volume_end:
             formatted_publisher_or_periodical = formatted_publisher_or_periodical + '-' + str(citation.part_details.volume_end)
-    if citation.part_details.issue_free_text:
+    if hasattr(citation.part_details,'issue_free_text') and citation.part_details.issue_free_text:
         formatted_publisher_or_periodical = formatted_publisher_or_periodical + ', no. ' + citation.part_details.issue_free_text
-    elif citation.part_details.issue_begin:
+    elif hasattr(citation.part_details,'issue_begin') and citation.part_details.issue_begin:
         formatted_publisher_or_periodical = formatted_publisher_or_periodical + ', no. ' + str(citation.part_details.issue_begin)
-        if citation.part_details.issue_end:
+        if hasattr(citation.part_details,'issue_end') and citation.part_details.issue_end:
             formatted_publisher_or_periodical = formatted_publisher_or_periodical + '-' + str(citation.part_details.issue_end)
     return formatted_publisher_or_periodical
 
@@ -154,12 +156,12 @@ def _format_pages_or_isbn(citation):
         formatted_pages_or_isbn = '.'
     else:
         formatted_pages_or_isbn = (', ' if citation.type_controlled is 'CH' else ': ')
-        if citation.part_details.pages_free_text:
+        if hasattr(citation.part_details,'pages_free_text') and citation.part_details.pages_free_text:
             formatted_pages_or_isbn = formatted_pages_or_isbn + citation.part_details.pages_free_text
-        elif citation.part_details.page_begin:
-            formatted_pages_or_isbn = formatted_pages_or_isbn + citation.part_details.page_begin
-            if citation.part_details.page_end:
-                formatted_pages_or_isbn = formatted_pages_or_isbn + citation.part_details.page_end
+        elif hasattr(citation.part_details,'page_begin') and citation.part_details.page_begin:
+            formatted_pages_or_isbn = formatted_pages_or_isbn + str(citation.part_details.page_begin)
+            if hasattr(citation.part_details,'page_end') and citation.part_details.page_end:
+                formatted_pages_or_isbn = formatted_pages_or_isbn + str(citation.part_details.page_end)
         formatted_pages_or_isbn = formatted_pages_or_isbn + '.'
 
     return formatted_pages_or_isbn
