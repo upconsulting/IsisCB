d6b4fd63f62e05a91a7571826605d6fc8b196f67
Julia Damerow <jdamerow@users.noreply.github.com>
2024-02-05 22:07:55 -0500

    Chore/iexp 504 (#807)
    * [IEXP-504] created dockerfile and docker-compose file with explore, db, and ES
    
    * [IEXP-504] make sure db files are being ignored
    
    * [IEXP-504] ignore files and logs

---
+++ scripts/startup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python -m pip install -r requirements.txt
+cd isiscb
+python manage.py migrate
+
+redis-server --daemonize yes
+celery -A isiscb worker -l info --detach --logfile=/app/logs/celery.log --pidfile=
+python manage.py runserver 0.0.0.0:8000 > /app/logs/django.log
