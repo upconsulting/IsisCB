a05ee149b9538122efb43f72a9c1770b068e26e4
crispzips <pvieth@ou.edu>
2024-02-07 13:25:58 -0500

    [IEXP-488]

--- isiscb/curation/utils.py
+++ isiscb/curation/utils.py
@@ -39,24 +39,23 @@ def _format_contributors(citation):
     if not contribs:
         return ''
     
-    formatted_contributors = ''
     formatted_contributors = _build_name_last_first_html(contribs[0])
-    if len(contribs) == 1:
-        formatted_contributors = formatted_contributors + '.'
-    elif len(contribs) == 2:
-        formatted_contributors = formatted_contributors + 'and ' + _build_name_first_last_html(contribs[1]) + '.'
+    if len(contribs) == 2:
+        formatted_contributors = formatted_contributors + 'and ' + _build_name_first_last_html(contribs[1])
     elif len(contribs) == 3:
-        formatted_contributors = formatted_contributors + ', ' + _build_name_first_last_html(contribs[1]) + ', and ' + _build_name_first_last_html(contribs[2]) + '.'
+        formatted_contributors = formatted_contributors + ', ' + _build_name_first_last_html(contribs[1]) + ', and ' + _build_name_first_last_html(contribs[2])
     elif len(contribs) > 3:
         formatted_contributors = formatted_contributors + ', ' + _build_name_first_last_html(contribs[1]) + ', ' + _build_name_first_last_html(contribs[2]) + ', et al.'
-    
+
+    formatted_contributors = formatted_contributors + ('.' if formatted_contributors[-1] != '.' else '')
+
     return formatted_contributors
 
 def _build_name_last_first_html(contrib):
-    if " " in contrib:
-        return '<span style="font-variant: small-caps;">' + contrib.split(' ')[1] + '</span>, ' + contrib.split(' ')[0]
     if "," in contrib:
-        return '<span style="font-variant: small-caps;">' + contrib.split(',')[0] + '</span>, ' + contrib.split(',')[1]  
+        return '<span style="font-variant: small-caps;">' + contrib.split(',')[0] + '</span>, ' + contrib.split(',')[1]
+    if " " in contrib:
+        return '<span style="font-variant: small-caps;">' + contrib.split(' ')[1] + '</span>, ' + contrib.split(' ')[0]  
     return '<span style="font-variant: small-caps;">' + contrib + '</span>'
     
 def _build_name_first_last_html(contrib):
@@ -81,32 +80,35 @@ def _format_publisher_or_periodical(citation):
     - for books: "PublisherName,"
     - for chapters: " In TitleOfBookContainingTheChapter, edited by 
     """
-    formatted_publisher_or_periodical = ''
 
-    publishers = ACRelation.objects.filter(citation=citation, type_controlled=ACRelation.PUBLISHER).filter(public=True)
     if citation.type_controlled == Citation.BOOK: 
-        return publishers[0].authority.name + ',' if publishers and publishers[0].authority else ''
+        publishers = ACRelation.objects.filter(citation=citation, type_controlled=ACRelation.PUBLISHER, public=True)
+        return publishers.first().authority.name + ',' if publishers and publishers.first().authority else ''
     
-    containing_citation = CCRelation.objects.filter(object_id=citation.id, type_controlled=CCRelation.INCLUDES_CHAPTER, subject__public=True).filter(public=True)
     if citation.type_controlled == Citation.CHAPTER:
-        formatted_publisher_or_periodical = ' In <i>' + containing_citation[0].subject.title + '</i>' if containing_citation else ''
-        containing_citation_editor = containing_citation[0].subject.get_all_contributors[0].authority if containing_citation and containing_citation[0].subject.get_all_contributors else None
+        containing_citation = CCRelation.objects.filter(object_id=citation.id, type_controlled=CCRelation.INCLUDES_CHAPTER, subject__public=True, public=True)
+        formatted_publisher_or_periodical = ' In <i>' + containing_citation.first().subject.title + '</i>' if containing_citation else ''
+        containing_citation_editor = containing_citation.first().subject.get_all_contributors[0].authority if containing_citation and containing_citation.first().subject.get_all_contributors else None
         formatted_publisher_or_periodical = formatted_publisher_or_periodical + (', edited by <span style="font-variant: small-caps;">' + containing_citation_editor.name + '</span>' if containing_citation_editor else '')
         return formatted_publisher_or_periodical
     
     if citation.type_controlled == Citation.THESIS:
-        schools = ACRelation.objects.filter(citation=citation.id, type_controlled=ACRelation.SCHOOL).filter(public=True)
-        return 'Dissertation at ' + schools[0].authority.name if schools and schools[0].authority else ''
+        schools = ACRelation.objects.filter(citation=citation.id, type_controlled=ACRelation.SCHOOL, public=True)
+        return 'Dissertation at ' + schools.first().authority.name if schools and schools.first().authority else ''
     
-    periodicals = ACRelation.objects.filter(citation=citation, type_controlled=ACRelation.PERIODICAL).filter(public=True)
-    return _format_periodicals(citation, periodicals) if periodicals else ''
+    return _format_periodicals(citation)
 
-def _format_periodicals(citation, periodicals):
+def _format_periodicals(citation):
     """
     formats volume number and issue numbers for periodicals, 
     accounting for the fact that some records have data in multiple fields describing volume and issue numbers
     """
-    formatted_publisher_or_periodical = '<i>' + periodicals[0].authority.name + '</i> ' if periodicals[0].authority else ''
+
+    periodicals = ACRelation.objects.filter(citation=citation, type_controlled=ACRelation.PERIODICAL, public=True)
+    if not periodicals:
+        return ''
+    
+    formatted_publisher_or_periodical = '<i>' + periodicals.first().authority.name + '</i> ' if periodicals.first().authority else ''
     if citation.part_details:
         if citation.part_details.volume:
             formatted_publisher_or_periodical = formatted_publisher_or_periodical + citation.part_details.volume
@@ -132,14 +134,13 @@ def _format_date(citation, formatted_publisher_or_periodical):
     if citation.type_controlled == Citation.BOOK: 
             return ' ' + date.strftime('%Y') + '.'
     # otherwise, wrap the publication year in parentheses
-    return ('(' if formatted_publisher_or_periodical and formatted_publisher_or_periodical[-1] is ' ' else ' (') + date.strftime('%Y') + ')'
+    return ('(' if formatted_publisher_or_periodical and formatted_publisher_or_periodical[-1] == ' ' else ' (') + date.strftime('%Y') + ')'
 
 def _format_pages_or_isbn(citation):
-    formatted_pages_or_isbn = ''
     isbn = None
-    if citation.linkeddata_public:            
-        isbn = [linked_datum if linked_datum.type_controlled == 'ISBN' else None for linked_datum in citation.linkeddata_public][0]
-        
+    if citation.linkeddata_public:     
+        isbn = next((linked_datum for linked_datum in citation.linkeddata_public if linked_datum.type_controlled.name == 'ISBN'), None)
+    formatted_pages_or_isbn = '' 
     if citation.type_controlled == Citation.BOOK:
         if isbn:
             formatted_pages_or_isbn = formatted_pages_or_isbn + ' <span style="text-variant: small-caps;">ISBN</span>:' + isbn.universal_resource_name + '.'
