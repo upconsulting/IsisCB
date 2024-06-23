835f659392776707272e50f9011672dacae63ec3
Julia Damerow <julia.damerow@gmail.com>
2024-06-23 11:18:21 -0400

    [ES7-upgrade] trying to get celery to install

--- .ebextensions/scripts/01_pip_install.sh
+++ .ebextensions/scripts/01_pip_install.sh
@@ -6,7 +6,7 @@ if [ "$IS_DOCKER" != "true" ]; then
   echo 'Pip install...'
   #source /opt/python/run/venv/bin/activate
   source /var/app/venv/staging-LQM1lest/bin/activate
-  pip install --use-deprecated=legacy-resolver -r requirements.txt
+  pip install -r requirements.txt
 else
   echo 'The script 01_pip_install.sh is only executed in non-docker environments.'
 fi
--- .ebextensions/scripts/07_update_pip.sh
+++ .ebextensions/scripts/07_update_pip.sh
@@ -6,7 +6,7 @@ if [ "$IS_DOCKER" != "true" ]; then
   echo 'Updating pip...'
   #source /opt/python/run/venv/bin/activate
   source /var/app/venv/staging-LQM1lest/bin/activate
-  pip install -U pip
+  pip install pip==23.3.2
 else
   echo 'The script 07_update_pip.sh is only executed in non-docker environments.'
 fi
