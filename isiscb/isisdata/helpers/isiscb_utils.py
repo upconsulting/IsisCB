00a6dbf799027ad1d0919c9f6bda7097ea91364e
Julia Damerow <jdamerow@asu.edu>
2025-10-21 21:32:21 -0400

    [IEXP-585] cleaned up wikipedia image synopsis function

--- isiscb/isisdata/helpers/isiscb_utils.py
+++ isiscb/isisdata/helpers/isiscb_utils.py
@@ -10,3 +10,13 @@ def generate_search_key(path):
     """
     md5_hash = hashlib.md5(path.encode())
     return base64.b64encode(md5_hash.hexdigest().encode()).decode(errors="ignore")
+
+def build_name_first_last(name):
+    if name.find(',') >= 0:
+        firstName = name[name.index(',')+1:len(name)].strip()
+        if firstName.find(',') >= 0:
+            firstName = firstName[:firstName.find(',')].strip()
+        lastName = name[:name.find(',')].strip()
+        name = firstName + ' ' + lastName
+        
+    return name
